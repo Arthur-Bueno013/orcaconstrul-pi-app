74b51b69eb5480895f7666147e40de03186f1898
jrvieirapr <juliano.vieira@docente.pr.senac.br>
2023-09-21 15:15:49 -0300

    Hello World

---
+++ app/Http/Controllers/UnidadeMedidaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UnidadeMedida;
+use App\Http\Requests\StoreUnidadeMedidaRequest;
+use App\Http\Requests\UpdateUnidadeMedidaRequest;
+
+class UnidadeMedidaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUnidadeMedidaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UnidadeMedida $unidadeMedida)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UnidadeMedida $unidadeMedida)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUnidadeMedidaRequest $request, UnidadeMedida $unidadeMedida)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UnidadeMedida $unidadeMedida)
+    {
+        //
+    }
+}
